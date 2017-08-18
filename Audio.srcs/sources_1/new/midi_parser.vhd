f783eece217f6ea1eea136eb35505871e7353e30
htistud <htistud@et.uni-magdeburg.de>
2017-08-18 12:29:33 +0200

    Revert "Fix note 0 being applied when no current note was found."
    This reverts commit 29a7f6d7a06771d51feb47683774d7c05f35a2e7.

--- Audio.srcs/sources_1/new/midi_parser.vhd
+++ Audio.srcs/sources_1/new/midi_parser.vhd
@@ -1,7 +1,36 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.08.2017 11:34:23
+-- Design Name: 
+-- Module Name: midi_parser - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use work.typedefs.all;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity midi_parser is
     Port ( rxData : in STD_LOGIC_VECTOR (7 downto 0);
@@ -39,8 +68,7 @@ begin
                 
                 when STATE_BYTE1 =>
                     cstate<=STATE_BYTE2;
-                    --parsedPitch<=note_empty; we need to keep the last note on note off.
-                    --otherwise the mixer will overflow
+                    parsedPitch<=note_empty;
                     if noteOff='0' then
                         if rxData(7) = '0' then
                             parsedPitch(6 downto 0) <= rxData(6 downto 0);
